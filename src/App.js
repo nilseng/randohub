3f7f8e61fb4219819ec12e24bcea9826f6ef13a3
Teodor Nilseng Danielsen <teodor.danielsen@gmail.com>
2020-05-22 22:21:18 +0200

    adding procfile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "react-bootstrap/Navbar";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import synshorn_img from "./images/synshorn.jpg";
+import mugnetind_img from "./images/mugnetind.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar bg="dark" expand="md" collapseOnSelect>
+        <NavBar.Brand style={{ color: "#f8f9fa" }}>
+          Rando
+          <span
+            style={{
+              backgroundColor: "orange",
+              paddingTop: "0.1rem",
+              paddingBottom: "0.1rem",
+              paddingRight: "0.2rem",
+              borderRadius: "0.1rem",
+              color: "black",
+            }}
+          >
+            hub
+          </span>
+        </NavBar.Brand>
+      </NavBar>
+      <div
+        style={{ width: "100vw", minHeight: "100vh", backgroundColor: "black" }}
+      >
+        <Container style={{ padding: "1rem" }}>
+          <Card
+            style={{ padding: "1rem", margin: "1rem", width: "27rem" }}
+            bg="dark"
+          >
+            <Card.Img
+              src={synshorn_img}
+              style={{ width: "25rem", paddingBottom: "1rem" }}
+            />
+            <Card.Title style={{ color: "#f8f9fa" }}>Synshorn 1475</Card.Title>
+            <Card.Body style={{ color: "#f8f9fa" }}>
+              Det regnet og var nice
+            </Card.Body>
+          </Card>
+          <Card
+            style={{ padding: "1rem", margin: "1rem", width: "27rem" }}
+            bg="dark"
+          >
+            <Card.Img
+              src={mugnetind_img}
+              style={{ width: "25rem", paddingBottom: "1rem" }}
+            />
+            <Card.Title style={{ color: "#f8f9fa" }}>Mugnetind 1740</Card.Title>
+            <Card.Body style={{ color: "#f8f9fa" }}>
+              Det regnet ikke og var nice
+            </Card.Body>
+          </Card>
+        </Container>
+      </div>
+    </>
   );
 }
 
